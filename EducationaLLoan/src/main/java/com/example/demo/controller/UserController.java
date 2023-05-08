c6c1cc860539632e954a82d2e2962f8e558a885e
yokeshgovindaraj021003 <131338855+yokeshgovindaraj021003@users.noreply.github.com>
2023-05-08 15:30:08 +0530

    Update UserController.java

--- EducationaLLoan/src/main/java/com/example/demo/controller/UserController.java
+++ EducationaLLoan/src/main/java/com/example/demo/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -44,5 +45,11 @@ public class UserController {
 		return gbi;
 		
 	}
+	@DeleteMapping("deleteprofile/{id}")
+	public void deleteProfile(@PathVariable("id") int id)
+	{
+		userSer.deleteProfile(id);
+	}
+delete in usercontro
 
 }
