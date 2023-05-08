400c0e8b123341e14e3de7a300c001928c2e942e
yokeshgovindaraj021003 <131338855+yokeshgovindaraj021003@users.noreply.github.com>
2023-05-08 14:56:46 +0530

    loan project

---
+++ EducationaLLoan/src/main/java/com/example/demo/EducationaLLoanApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationaLLoanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducationaLLoanApplication.class, args);
+	}
+
+}
